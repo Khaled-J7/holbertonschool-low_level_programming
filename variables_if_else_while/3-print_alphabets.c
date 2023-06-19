94a0dce83144dbc4bbb63782c73dc0b61a7162a8
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-06-19 22:14:18 +0100

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,18 @@
  */
 int main(void)
 {
-	char c;
-	char a;
-	char z;
-	char A;
-	char Z;
+	char letter;
 
-	for (c = 'a' ; c <= z ; c++)
+	for (letter = 'a' ; letter <= z ; letter++)
 
 	{
-		putchar(c);
+		putchar(letter);
 	}
 
-	for (c = 'A' ; c <= 'Z' ; c++)
+	for (letter = 'A' ; letter <= 'Z' ; letter++)
 
 	{
-		putchar(c);
+		putchar(letter);
 	}
 	putchar('\n');
 
