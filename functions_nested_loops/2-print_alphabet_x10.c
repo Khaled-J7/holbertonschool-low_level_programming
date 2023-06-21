ac7eacac73ef34063cd193fd8b58c4308d23e53a
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-06-21 23:38:22 +0100

    x10 alpha

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,19 @@
 */
 void print_alphabet_x10(void)
 {
-	char letter;
+	char ch;
 	int i;
 
 	i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		ch = 'a';
+		while (ch <= 'z')
 
 		{
-			_putchar('letter');
-			letter++;
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
 		i++;
