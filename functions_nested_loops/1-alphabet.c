60ab7121263869f2d48e89faee1f951ac81cdc9a
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-06-21 13:10:23 +0100

    1st task

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - print all alpha
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
