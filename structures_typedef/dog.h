9328e51535b553212a97b2e4a163984d26ba6596
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-07-18 03:39:41 -0700

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,19 +8,13 @@
  * @owner: Third member
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
